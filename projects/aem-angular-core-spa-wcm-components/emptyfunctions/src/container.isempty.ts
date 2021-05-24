ee283799c4547d8e91dace83489316b3b1a1e126
Niek Raaijmakers <raaijmak@adobe.com>
2021-05-24 09:01:01 +0200

    Fix unit test

--- projects/aem-angular-core-spa-wcm-components/emptyfunctions/src/container.isempty.ts
+++ projects/aem-angular-core-spa-wcm-components/emptyfunctions/src/container.isempty.ts
@@ -1,5 +1,5 @@
-import {ContainerModel} from "./models";
+import {ContainerProperties} from "./models";
 
-export function ContainerIsEmptyFn(props:ContainerModel){
-    return props[":itemsOrder"] == null || props[":itemsOrder"].length === 0;
+export function ContainerIsEmptyFn(props:ContainerProperties){
+    return props.cqItemsOrder == null || props.cqItemsOrder.length === 0;
 }
